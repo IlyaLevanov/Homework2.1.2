aed3c4d2fe4bf7d0eb1d068452a4c638786264e3
Ilya Levanov <ilya.levanov@inbox.ru>
2021-09-17 23:35:36 +0300

    added circle corners and completed all logic, task is done

--- Homework2.1.2/ViewController.swift
+++ Homework2.1.2/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let lightIsOff: CGFloat = 0.4
+    let lightIsOn: CGFloat = 1
+    
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeCircleView(view: redLight)
+        makeCircleView(view: yellowLight)
+        makeCircleView(view: greenLight)
+
+        switchButton.layer.cornerRadius = 10
+        switchButton.layer.masksToBounds = true
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
     }
-
-
+    
+    @IBAction func switchButtonPress() {
+        if switchButton.title(for: .normal) == "START"{
+            redLight.alpha = lightIsOn
+            switchButton.setTitle("NEXT", for: .normal)
+            return
+        }
+        
+        if greenLight.alpha == lightIsOn{
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+        } else if yellowLight.alpha == lightIsOn{
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+        } else{
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+        }
+                
+    }
+    
+    func makeCircleView(view: UIView){
+        view.layer.cornerRadius = view.bounds.size.width/2
+    }
+    
+    
 }
+    
 
